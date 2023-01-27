d3c85439d2c2661bc476c8255ead8050c2df2ddf
ljn425 <ljn425@naver.com>
2023-01-27 22:37:31 +0900

    AppConfig Class 추가

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,42 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP 는 10% 할인이 적용되어야 한다.")
+    void discount() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+
+        // when
+        int result = discountPolicy.discount(member, 10000);
+
+
+        //then
+        assertThat(result).isEqualTo(1000);
+
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지않아야 한다.")
+    void vip_x() {
+        Member member = new Member(2L, "memberB", Grade.BASIC);
+
+        int result = discountPolicy.discount(member, 10000);
+
+        assertThat(result).isEqualTo(0);
+
+    }
+}
